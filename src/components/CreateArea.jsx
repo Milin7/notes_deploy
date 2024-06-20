88f9bbe910fc5fa84a00e88eb3cdd22532f88e41
Milin7 <isaac_martinez.g@hotmail.com>
2024-06-20 12:10:22 -0600

    first commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
+import Collapse from "@mui/material/Collapse";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [isExpanded, setExpanded] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+  function expand() {
+    setExpanded(true);
+  }
+  function mouseIsOver() {
+    setExpanded(true);
+  }
+  function mouseIsOut() {
+    setExpanded(false);
+  }
+
+  return (
+    <div
+      className="notepad"
+      onMouseOver={mouseIsOver}
+      onMouseLeave={mouseIsOut}
+    >
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
